fa296f21da14949aa990c5612cd36e869399f5b8
jm-molins <josep.molins@schibsted.com>
2025-06-11 11:17:04 +0200

    Añadido Google Analytics

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -20,13 +20,16 @@ export default function RootLayout({
   return (
     <html lang={locale}>
       <head>
-        <Script id="google-tag-manager" strategy="afterInteractive">
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-WR7S4N71T4"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5W4SH8K9');
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WR7S4N71T4');
           `}
         </Script>
       </head>
