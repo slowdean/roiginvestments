677260c392c3b3ebf25fe4f60a37d7839fac5d4f
jm-molins <josep.molins@schibsted.com>
2025-06-11 09:33:51 +0200

    Añadido Google Tag Manager

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Script from 'next/script'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Roig Investments',
+  description: 'Tu socio de confianza en inversiones inmobiliarias',
+}
+
+export default function RootLayout({
+  children,
+  params: { locale }
+}: {
+  children: React.ReactNode
+  params: { locale: string }
+}) {
+  return (
+    <html lang={locale}>
+      <head>
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5W4SH8K9');
+          `}
+        </Script>
+      </head>
+      <body className={inter.className}>
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5W4SH8K9"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {children}
+      </body>
+    </html>
+  )
+} 
